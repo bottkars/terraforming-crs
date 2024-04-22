7249ef84b9638948d07368ebf7936b652046b75e
bottkars <karsten.bott@bottnet.de>
2024-04-22 09:47:35 +0200

    ppcr

--- terraforming-azure/modules/ppcr/ppcr.tf
+++ terraforming-azure/modules/ppcr/ppcr.tf
@@ -93,7 +93,7 @@ resource "azurerm_network_interface" "ppcr_nic" {
 
 
 resource "azurerm_storage_account" "ppcr_diag_storage_account" {
-  name                     = "${var.resourcePrefix}-CR-VM-diag${random_string.storage_account_name.result}"
+  name                     = "ppcrvmdiag${random_string.storage_account_name.result}"
   resource_group_name              = data.azurerm_resource_group.ppcr_resource_group.name
   location                         = data.azurerm_resource_group.ppcr_resource_group.location
   account_tier             = "Standard"
