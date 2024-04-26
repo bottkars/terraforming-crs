63e5c30ecd299af2f144fbd513dc4d7f9858d3de
bottkars <karsten.bott@bottnet.de>
2024-04-26 16:11:10 +0200

    jh step 1

--- terraforming-azure/ppcr_outputs.tf
+++ terraforming-azure/ppcr_outputs.tf
@@ -7,3 +7,8 @@ output "ppcr_ssh_private_key" {
   sensitive = true
   value     = module.ppcr.ssh_private_key 
 }
+
+output "ppcr_private_ip" {
+  value       = module.ppcr.ppcr_private_ip_address
+  description = "The private ip address for all ppcr"
+}
