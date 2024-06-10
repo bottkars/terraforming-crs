d9a2d9dfe9df1d166515c8656ed5e9c8a817fe30
bottkars <karsten.bott@bottnet.de>
2024-06-10 14:59:58 +0200

    private endpoint

--- terraforming-azure/modules/ddve/endpoint.tf
+++ terraforming-azure/modules/ddve/endpoint.tf
@@ -1,7 +1,7 @@
 resource "azurerm_private_endpoint" "blobendpoint" {
   name                = "${var.resourcePrefix}-privateendpoint"
-  location            = data.azurerm_resource_group.ddve_networks_resource_group.location
-  resource_group_name = data.azurerm_resource_group.ddve_networks_resource_group.name
+  location            = data.azurerm_resource_group.ddve_resource_group.location
+  resource_group_name = data.azurerm_resource_group.ddve_resource_group.name
   subnet_id           = var.management_subnet_id
 
   private_service_connection {
