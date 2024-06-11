0a69c91374d67e08dcea65c0c2ee9b0868d35239
bottkars <karsten.bott@bottnet.de>
2024-06-11 13:21:10 +0200

    private endpoint

--- terraforming-azure/ddve_outputs.tf
+++ terraforming-azure/ddve_outputs.tf
@@ -2,3 +2,7 @@ output "ddve_private_ip" {
   value       = module.ddve[0].ddve_private_ip_address
   description = "The private ip address for all jumphost"
 }
+
+output "atos_account" {
+value = module.ddve[0].atos_account
+}
