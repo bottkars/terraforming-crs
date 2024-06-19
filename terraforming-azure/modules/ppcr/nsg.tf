c593b3bfcfa863adc32fa30055cb3f7a9eb2288d
bottkars <karsten.bott@bottnet.de>
2024-06-19 09:10:44 +0200

    private endpoint

--- terraforming-azure/modules/ppcr/nsg.tf
+++ terraforming-azure/modules/ppcr/nsg.tf
@@ -132,18 +132,18 @@ resource "azurerm_network_security_group" "ppcr_security_group" {
   }
 
 
-  security_rule {
-    name                       = "Deny_All_Inbound"
-    priority                   = 4096
-    direction                  = "Inbound"
-    access                     = "Deny"
-    description                = "Deny All Inbound - Overrides Azure Allow All Default Rule"
-    protocol                   = "*"
-    source_port_range          = "*"
-    destination_port_range     = "*"
-    source_address_prefix      = "*"
-    destination_address_prefix = "*"
-  }
+#  security_rule {
+#    name                       = "Deny_All_Inbound"
+#    priority                   = 4096
+#    direction                  = "Inbound"
+#    access                     = "Deny"
+#    description                = "Deny All Inbound - Overrides Azure Allow All Default Rule"
+#    protocol                   = "*"
+#    source_port_range          = "*"
+#    destination_port_range     = "*"
+#    source_address_prefix      = "*"
+#    destination_address_prefix = "*"
+#  }
  # security_rule {
  #   name                       = "Deny_All_Outbound"
  #   priority                   = 4096
